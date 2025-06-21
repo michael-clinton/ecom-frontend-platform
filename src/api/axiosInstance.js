01d92604a88960862b065beb133b5902cb22d352
michael-clinton <michaelclinton084@gmail.com>
2025-06-21 22:05:22 +0530

    chore: update axiosInstance base URL and headers

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL || "http://localhost:5000/", // Use env variable or fallback to localhost
+  baseURL: "https://redstore-ecommerce-platform.onrender.com", // Use env variable or fallback to localhost
   headers: {
     "Content-Type": "application/json",
   },
